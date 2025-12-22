2f8e2ef429a5e3f61429d7d835ffeaa99d08d67a
RakkeshSavael <156323140+RakkeshIT@users.noreply.github.com>
2025-12-22 15:52:57 +0530

    Add Home Page Styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,50 @@
 'use client'
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
+import { Container, Box, Typography, Button } from '@mui/material';
+import TextPressure from '../components/TextPressure';
+import { RainbowButton } from '../components/ui/rainbow-button';
 export default function SignInCard() {
 
   return (
     <>
-      <h1>Home</h1>
+      <Box
+        sx={{
+          minHeight: '100vh',
+          background: "linear-gradient(to right top, #7604f2, #6608f0, #550bef, #3e0fed, #1b12eb)",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 4,
+        }}
+      >
+        <Container maxWidth="md"
+           sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 4,
+        }}
+        >
+       <TextPressure
+              text="Welcome to SmartCRUD!"
+              flex={true}
+              alpha={false}
+              stroke={false}
+              width={true}
+              weight={true}
+              italic={true}
+              textColor="#ffffff"
+              strokeColor="#ff0000"
+              minFontSize={30}
+            />
+
+          <RainbowButton className="mt-8 px-8" href="/register">
+            Create Your Account
+          </RainbowButton>
+        </Container>
+      </Box>
     </>
   );
 }
