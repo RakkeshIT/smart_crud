5a6c108f5f3e42b0559b53e14bafc5ed1aa60a62
RakkeshSavael <156323140+RakkeshIT@users.noreply.github.com>
2025-12-23 00:31:50 +0530

    Update page.tsx

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,4 +1,9 @@
+import React from 'react'
 
-export const ClientPage = () => {
-    return <div>Client Page</div>;
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
+
+export default page
