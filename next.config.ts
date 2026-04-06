9543e60222e0cf091bc3c057df68579c11b82d9d
RakkeshFireSky <rakkeshkumar@firesky.cc>
2026-04-06 19:22:22 +0530

    feat: add API rewrites to proxy requests to the backend service

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://smart-crud-umber.vercel.app/:path*", // 👈 Express backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
